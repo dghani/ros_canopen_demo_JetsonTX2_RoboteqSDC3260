7a712a87db4dc0b6f8e6e501b18a7b2e85027e98
New-Jetson <trickfirerobotics@gmail.com>
2019-02-16 15:37:57 -0800

    update start script

--- start_robot.sh
+++ start_robot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+LOGGING_ENABLED=true
 
 # Initialize CAN interface on an Nvidia Jetson TX2
 # Reference: https://devtalk.nvidia.com/default/topic/1025010/jetson-tx2/how-to-use-can-on-jetson-tx2-/
@@ -6,9 +7,14 @@ modprobe can
 modprobe can_raw
 modprobe mttcan
 ip link set can0 type can bitrate 500000 dbitrate 2000000 berr-reporting on fd on
-p link set up can0
+ip link set up can0
 
 source devel/setup.bash
 
-roslaunch demo_pkg demo.launch
+if [ LOGGING_ENABLED ]; then
+    CURRENTDATE=`date +"%m-%d-%Y_%T"`
+    roslaunch demo_pkg demo.launch 2>&1 | tee "log_$CURRENTDATE.txt"
+else
+    roslaunch demo_pkg demo.launch
+fi
 

04c0fda80f369674ad458c6119a5ab08f018dd2c
New-Jetson <trickfirerobotics@gmail.com>
2019-02-16 15:04:25 -0800

    change start_robot.sh to initialize CAN according to known good instructions.

--- start_robot.sh
+++ start_robot.sh
@@ -1,5 +1,12 @@
-sudo modprobe mttcan
-sudo ip link set can0 up type can bitrate 500000
+#!/bin/bash
+
+# Initialize CAN interface on an Nvidia Jetson TX2
+# Reference: https://devtalk.nvidia.com/default/topic/1025010/jetson-tx2/how-to-use-can-on-jetson-tx2-/
+modprobe can
+modprobe can_raw
+modprobe mttcan
+ip link set can0 type can bitrate 500000 dbitrate 2000000 berr-reporting on fd on
+p link set up can0
 
 source devel/setup.bash
 
